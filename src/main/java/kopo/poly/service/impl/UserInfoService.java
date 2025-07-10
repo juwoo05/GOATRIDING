1f0d2007520a2779bda5e74f56110484b5a8f4d8
Hello <jijuwoo@gmail.com>
2025-07-10 17:38:13 +0900

    지주우 메일

---
+++ src/main/java/kopo/poly/service/impl/UserInfoService.java
@@ -0,0 +1,116 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.dto.MailDTO;
+import kopo.poly.dto.UserInfoDTO;
+import kopo.poly.mapper.IUserInfoMapper;
+import kopo.poly.service.IMailService;
+import kopo.poly.service.IUserInfoService;
+import kopo.poly.util.CmmUtil;
+import kopo.poly.util.EncryptUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserInfoService implements IUserInfoService {
+
+    private final IUserInfoMapper userInfoMapper;
+
+    private final IMailService mailService;
+
+    @Override
+    public UserInfoDTO getUserIdExists(UserInfoDTO pDTO) throws Exception {
+
+        log.info("{}.getUserIdExists Start!", this.getClass().getName());
+
+        UserInfoDTO rDTO = userInfoMapper.getUserIdExists(pDTO);
+
+        log.info("{}.getUserIdExists End!", this.getClass().getName());
+
+        return rDTO;
+    }
+
+    @Override
+    public UserInfoDTO getEmailExists(UserInfoDTO pDTO) throws Exception {
+
+        log.info("{}.emailAuth Start!", this.getClass().getName());
+
+        UserInfoDTO rDTO = Optional.ofNullable(userInfoMapper.getEmailExists(pDTO)).orElseGet(UserInfoDTO::new);
+
+        log.info("rDTO : {}", rDTO);
+
+        if (CmmUtil.nvl(rDTO.getExistsYn()).equals("N")) {
+
+            // 6자리 랜덤 숫자 생성하기
+            int authNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
+
+            log.info("authNumber : {}", authNumber);
+
+            // 인증번호 발송 로직
+            MailDTO dto = new MailDTO();
+
+            dto.setTitle("이메일 중복 확인 인증번호 발송 메일");
+            dto.setContents("인증번호는 " + authNumber + " 입니다.");
+            dto.setToMail(EncryptUtil.decAES128CBC(CmmUtil.nvl(pDTO.getEmail())));
+
+            mailService.doSendMail(dto); // 이메일 발송
+
+            dto = null;
+
+            rDTO.setAuthNumber(authNumber); // 인증번호를 결과값에 넣어주기
+        }
+
+        log.info("{}.emailAuth End!", this.getClass().getName());
+
+        return rDTO;
+    }
+
+    @Override
+    public int insertUserInfo(UserInfoDTO pDTO) throws Exception {
+
+        log.info("{}.inserUserInfo Start!", this.getClass().getName());
+
+        int res;
+
+        int success = userInfoMapper.insertUserInfo(pDTO);
+
+        if (success > 0) { // DB에 아이디가 등록되었다면(회원정보 입력에 성공했다면...)
+            res = 1;
+
+    /*
+        #######################################################################
+        #                             메일 발송 로직 작동 시작!!                              #
+        #######################################################################
+     */
+            MailDTO mDTO = new MailDTO();
+
+            // 회원정보입력완료되면 입력한 이메일로 회원정보가 입력되었으며 회원의 이름과 회원의 환영 메시지 전송
+            mDTO.setToMail(EncryptUtil.decAES128CBC(CmmUtil.nvl(pDTO.getEmail())));
+
+            mDTO.setTitle("회원가입을 축하드립니다."); // 제목
+
+            // 사용자 이름 가져와서 메시지 내용 설정
+            mDTO.setContents(CmmUtil.nvl(pDTO.getUserName()) + " 님의 회원가입을 진심으로 축하드립니다.");
+
+            mailService.doSendMail(mDTO); // 메일 발송
+
+    /*
+        #######################################################################
+        #                             메일 발송 로직 작동 끝!!                                #
+        #######################################################################
+     */
+
+        } else {
+            res = 0;
+        }
+
+        log.info("{}.inserUserInfo End!", this.getClass().getName());
+
+        return res;
+    }
+}
