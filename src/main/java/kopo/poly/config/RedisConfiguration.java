fcefd86f78e99d735af37fcb0d3f2b08f9982e79
Hello <jijuwoo@gmail.com>
2025-10-29 15:50:58 +0900

    9/08

--- src/main/java/kopo/poly/config/RedisConfiguration.java
+++ src/main/java/kopo/poly/config/RedisConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+
 @Configuration
 public class RedisConfiguration {
 
